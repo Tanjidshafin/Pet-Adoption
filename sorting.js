fb35de05bb4afdf95856ac00f0d88ba82de6c69e
Tanjid Karim <you@example.com>
2024-10-08 17:31:23 +0600

    75% done

---
+++ sorting.js
@@ -0,0 +1,14 @@
+const items = [
+
+    { name: "Item 1", price: 100 },
+
+    { name: "Item 2", price: 50 },
+
+    { name: "Item 3", price: 200 },
+
+    { name: "Item 4", price: 150 },
+
+];
+console.log(items.sort((a,b)=>a.price-b.price));
+
+ 
